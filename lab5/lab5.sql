1612a6252589dd3633bc65b0f532214efd7b73b1
tamirlanm <tamirlanmirgali@gmail.com>
2024-11-06 02:04:33 +0500

    lab6

--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -2,33 +2,33 @@
 create database lab5;
 --2
 create table salesman(
-    salesman_id int not null primary key,
+    salesman_id int primary key,
     name varchar(250),
     city varchar(250),
-    commission double precision not null
+    commission double precision
 );
 create table customers(
-    costumer_id int primary key not null,
+    costumer_id int primary key ,
     cust_name varchar(250),
     city text,
-    grade int not null,
-    salesman_id int not null
+    grade int ,
+    salesman_id int
 );
 create table orders(
-    ord_no int not null,
-    purch_amt double precision not null,
+    ord_no int ,
+    purch_amt double precision,
     ord_date date,
-    costumer_id int not null,
-    salesman_id int not null
+    costumer_id int,
+    salesman_id int
 );
 insert into salesman(salesman_id, name, city, commission) values(5001,'James Hoog', 'New York',0.15),
                                                                 (5002,'Nail Knite', 'Paris', 0.13),
                                                                 (5005,'Pit Alex', 'London', 0.11),
-                                                                (5006,'Mc Lyon', 'London',0.12),
+                                                                (5006,'Mc Lyon', null,0.12),
                                                                 (5007,'Paul Adam', 'Rome', 0.13);
 insert into customers(costumer_id, cust_name, city, grade, salesman_id) values(3002,'Nick Rimando', 'New York', 100, 5001),
                                                                               (3005,'Graham Zusi','California',200,5002),
-                                                                              (3001,'Brad Guzan','London',100 ,5005),
+                                                                              (3001,'Brad Guzan','London',null,5005),
                                                                               (3004,'Fabian Johns','Paris',300,5006),
                                                                               (3007,'Brad Davis', 'New York', 200,5001),
                                                                               (3009,'Geoff Camero','Berlin',100,5003),
@@ -54,11 +54,11 @@ select count(customers.cust_name) as total_costumers from customers;
 select min(orders.purch_amt) from orders;
 
 --7
-select cust_name from customers where cust_name like '%b';
+select cust_name from customers where cust_name like 'k%';
 
 --8
 select o.* from orders o join customers c on o.costumer_id = c.costumer_id
-where c.city = 'New York';
+where c.city <> 'New York';
 
 --9
 select c.* from customers c join orders o on c.costumer_id = o.costumer_id
---
+++ lab6/lab6.sql
@@ -0,0 +1,95 @@
+--task1
+create database lab6;
+
+--task2
+create table locations(
+    location_id serial primary key,
+    street_address varchar(25),
+    postal_code varchar(12),
+    city varchar(30),
+    state_province varchar(12)
+);
+create table departments(
+    department_id serial primary key,
+    department_name varchar(50) unique,
+    budget integer,
+    location_id integer references locations
+);
+create table employees(
+    employee_id serial primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(50),
+    phone_number varchar(20),
+    salary integer,
+    department_id integer references departments
+);
+insert into locations(street_address, postal_code, city, state_province)
+values( '123 Main Street','12345','New York','NY'),
+      ('456 Oak Avenue','67890','Los Angeles','CA'),
+      ('789 Elm Street','56789','Chicago','IL');
+
+insert into departments(department_name, budget, location_id)
+values ('Sales',10000,1),
+       ('Marketing',75000,2),
+       ('Engineering',125000,3);
+
+insert into employees(first_name, last_name, email, phone_number, salary, department_id)
+values ('John','Doe','john.doe@gmail.com','555-123-4567',50000,1),
+       ('Jane','Smith','jane.smith@gmail.com','555-234-5678',60000,2),
+       ('Alice','Johnson','alice.johnson@gmail.com','555-345-6789',70000,3);
+
+--task3
+select
+    e.first_name,
+    e.last_name,
+    e.department_id,
+    d.department_name
+from
+    employees e
+inner join departments d on d.department_id = e.department_id;
+
+--task4
+SELECT
+    e.first_name,
+    e.last_name,
+    e.department_id,
+    d.department_name
+FROM
+    employees e
+INNER JOIN
+    departments d ON e.department_id = d.department_id
+WHERE
+    d.department_id IN (80, 40);
+
+--task5
+select
+    e.first_name,
+    e.last_name,
+    d.department_name,
+    l.city,
+    l.state_province
+from
+    employees e
+inner join
+    departments d on e.department_id = d.department_id
+inner join
+    locations l on d.location_id = l.location_id;
+
+--task6
+select d.department_name,
+       count(e.employee_id) as employee_count
+from
+    departments d
+left join employees e on d.department_id = e.department_id
+group by d.department_name;
+
+--task7
+select
+    e.first_name,
+    e.last_name,
+    e.department_id,
+    d.department_name
+from
+    employees e
+left join departments d on d.department_id = e.department_id;
